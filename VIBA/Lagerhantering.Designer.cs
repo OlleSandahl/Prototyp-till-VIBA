d3c0d92d52920be1609a14110f0131aebff8a5d1
Jonatan <jonatan.gronhaug@live.se>
2020-05-18 18:46:20 +0200

    Senaste

--- VIBA/Lagerhantering.Designer.cs
+++ VIBA/Lagerhantering.Designer.cs
@@ -29,14 +29,16 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
             this.Artikelnummer = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Artikelnamn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Hylla = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Antal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Siffra2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Siffra1 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,46 +54,6 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(450, 126);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(16, 32);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(91, 30);
-            this.listBox1.TabIndex = 33;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.CausesValidation = false;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 9);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(196, 20);
-            this.label4.TabIndex = 34;
-            this.label4.Text = "Antal ordrar i kö just nu";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.CausesValidation = false;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 89);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(144, 20);
-            this.label1.TabIndex = 35;
-            this.label1.Text = "Aktuell plocklista";
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(16, 244);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 32);
-            this.button1.TabIndex = 36;
-            this.button1.Text = "Vara plockad";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Artikelnummer
             // 
@@ -113,15 +75,78 @@
             this.Antal.HeaderText = "Antal";
             this.Antal.Name = "Antal";
             // 
+            // Siffra2
+            // 
+            this.Siffra2.FormattingEnabled = true;
+            this.Siffra2.Location = new System.Drawing.Point(239, 42);
+            this.Siffra2.Name = "Siffra2";
+            this.Siffra2.Size = new System.Drawing.Size(91, 30);
+            this.Siffra2.TabIndex = 33;
+            this.Siffra2.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.CausesValidation = false;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(237, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Ordrar i kö";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.CausesValidation = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 20);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Aktuell plocklista";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 32);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Vara plockad";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.CausesValidation = false;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "PlockID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Siffra1
+            // 
+            this.Siffra1.FormattingEnabled = true;
+            this.Siffra1.Location = new System.Drawing.Point(20, 42);
+            this.Siffra1.Name = "Siffra1";
+            this.Siffra1.Size = new System.Drawing.Size(91, 30);
+            this.Siffra1.TabIndex = 37;
+            this.Siffra1.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
             // Lagerhantering
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(488, 288);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Siffra1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Siffra2);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Lagerhantering";
             this.Text = "Lagerhantering";
@@ -135,7 +160,7 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox Siffra2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
@@ -143,5 +168,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Artikelnamn;
         private System.Windows.Forms.DataGridViewTextBoxColumn Hylla;
         private System.Windows.Forms.DataGridViewTextBoxColumn Antal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox Siffra1;
     }
 }
--- VIBA/Lagerhantering.cs
+++ VIBA/Lagerhantering.cs
@@ -26,5 +26,15 @@ namespace VIBA
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
